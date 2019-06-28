1e848ab27485c813ebccb526d5831f7adcdef9cd
mfoydl <31784414+mfoydl@users.noreply.github.com>
2019-06-28 02:14:03 -0400

    Created functions to allow enabling and disabling of TextBoxes

--- Equipment_Log/MainWindow.xaml.cs
+++ Equipment_Log/MainWindow.xaml.cs
@@ -21,5 +21,49 @@ namespace Equipment_Log {
         public MainWindow() {
             InitializeComponent();
         }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject {
+            if (depObj != null) {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++) {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T) {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child)) {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disables all TextBoxes in the current log
+        /// Called when user submits the log so changes cannot be made afterwards
+        /// </summary>
+        private void DisableComponents(object sender, RoutedEventArgs e) {
+            Console.WriteLine("Disable Event Fired");
+            var lstControl = FindVisualChildren<TextBox>(this);
+
+            foreach (TextBox control in lstControl) {
+                ///Disable Control if it is a textbox
+                control.IsReadOnly = true;
+            }
+        }
+
+        private void EnableComponents(object sender, RoutedEventArgs e) {
+            Console.WriteLine("Enable Event Fired");
+
+            var lstControl = FindVisualChildren<TextBox>(this);
+
+            foreach (TextBox control in lstControl) {
+                //Renable Textboxes
+                control.IsReadOnly = false;
+            }
+        }
+
+        private void TestEvent(object sender, RoutedEventArgs e) {
+      
+        }
     }
 }
